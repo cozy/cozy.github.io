36dd8747457c9c292572c88b95c5c5c7aa4f816f
Patrick Browne <pt.browne@gmail.com>
2018-04-04 12:04:47 +0200

    fix: hide curl output

--- deploy.sh
+++ deploy.sh
@@ -43,7 +43,7 @@ RUNDECK_TOKEN=${RUNDECK_TOKEN:-""}
 
 if [[ ! -z "$RUNDECK_UPDATE_DOCS_JOB_URL" &&  ! -z $RUNDECK_TOKEN ]]; then
     echo "Updating on docs.cozy.io via Rundeck..."
-    curl --fail -X POST -H "X-Rundeck-Auth-Token: $RUNDECK_TOKEN" $RUNDECK_UPDATE_DOCS_JOB_URL
+    curl -s --fail -X POST -H "X-Rundeck-Auth-Token: $RUNDECK_TOKEN" $RUNDECK_UPDATE_DOCS_JOB_URL > /dev/null
     echo "Should be visible on https://docs.cozy.io/"
 else
     echo "No Rundeck env vars (RUNDECK_UPDATE_DOCS_JOB_URL or RUNDECK_TOKEN), cannot deploy on docs.cozy.io"
