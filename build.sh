b0a70d96edb81c03f75e68d25d3ac22bf47894b2
Clochix <clochix@clochix.net>
2018-06-20 16:02:18 +0200

    [dev] Override french dev doc with the english one
    We only want english version of the developer documentation, but mkdocs
    doesn't provide an easy way to achieve this so we override the french
    pages with the english ones after build.

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ python add_external_docs.py
 mkdocs build -f mkdocs.yml
 mkdocs build -f mkdocs_fr.yml
 msgmerge --update i18n/fr_FR/LC_MESSAGES/messages.po i18n/messages.pot
+
+# Override french developer documentation with the up to date english version
+cp -R docs/en/dev/ docs/fr/
+
