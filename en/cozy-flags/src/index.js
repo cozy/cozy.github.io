e9e4d884ab34c4636d1965253ca268fbaefca08e
Travis CI User <travis@example.org>
2018-12-10 10:12:48 +0000

    publish: Merge pull request #111 from bobsaintcool/dev
    generated from commit 699b011c941c263536198575eb0b95be4293ff9b

--- en/cozy-flags/src/index.js
+++ en/cozy-flags/src/index.js
@@ -1,6 +1,8 @@
 export { default as FlagSwitcher } from './browser/FlagSwitcher'
 
-const flag = global
+const isNode = global && global.process && global.process.title === 'node'
+
+const flag = isNode
   ? require('./node/flag').default
   : require('./browser/flag').default
 
