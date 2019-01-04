eba54cd1f0f9acbaa455c8d588d0a0637e4d22cf
Travis CI User <travis@example.org>
2019-01-04 09:24:13 +0000

    publish: Merge pull request #117 from cozy/fix/auth-nointernal
    generated from commit db6eb3e167479ccd61e1a94fba28b05dfef66c1a

--- en/cozy-flags/src/index.js
+++ en/cozy-flags/src/index.js
@@ -1,8 +1,14 @@
+/* global __ENABLED_FLAGS__ */
+
 import isNode from 'detect-node'
 export { default as FlagSwitcher } from './browser/FlagSwitcher'
 
 const flag = isNode
   ? require('./node/flag').default
   : require('./browser/flag').default
+
+if (__ENABLED_FLAGS__ && Array.isArray(__ENABLED_FLAGS__)) {
+  __ENABLED_FLAGS__.forEach(enabledFlag => flag(enabledFlag, true))
+}
 
 export default flag
