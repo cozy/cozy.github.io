96045839a5cfc987a0b8a3efa258ceb6a3ce8a32
Travis CI User <travis@example.org>
2019-07-04 10:52:07 +0000

    publish: docs: Update app documentation about node v10 usage
    generated from commit 6a5afa355f1a884355fc6498ed91340757e6e833

--- en/cozy-flags/src/flag-codemod.js
+++ en/cozy-flags/src/flag-codemod.js
@@ -8,6 +8,10 @@ const isBlockLike = path => {
   return path.value && path.value.length !== undefined && path.name === 'body'
 }
 
+const countJSX = (root, j) => {
+  return root.find(j.JSXOpeningElement).length
+}
+
 /** Removes unused imports by counting usage */
 const removeUnusedImports = (root, j) => {
   const importsToRemove = root.find(j.ImportDeclaration)
@@ -16,7 +20,8 @@ const removeUnusedImports = (root, j) => {
     for (let specifier of path.node.specifiers) {
       const identifierName = specifier.local.name
       const usages = root.find(j.Identifier, { name: identifierName })
-      const nUsage = usages.size()
+      const nUsage =
+        usages.size() + (identifierName === 'React' ? countJSX(root, j) : 0)
       // import { toto } from 'lib' counts as 2 usages of toto
       // import toto from 'lib' counts a 1 usage of toto
       const importUsages =
@@ -29,7 +34,7 @@ const removeUnusedImports = (root, j) => {
         toKeep.push(specifier)
       }
     }
-    if (toKeep.length === 0) {
+    if (toKeep.length === 0 && path.node.specifiers.length > 0) {
       path.replace(null)
     } else {
       path.node.specifiers = toKeep
