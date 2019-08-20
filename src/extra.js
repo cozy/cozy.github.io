1090250a8fe0af14671119ab4bc9a09598f056c2
Patrick Browne <pt.browne@gmail.com>
2019-08-20 15:52:20 +0200

    fix: Repair edit link
    Previously was implemeted inside template

--- src/extra.js
+++ src/extra.js
@@ -27,7 +27,43 @@ var setupSearchHotkey = function () {
   document.addEventListener('keydown', checkForSearchSlash)
 }
 
+const fixEditLinkIfNecessary = function () {
+  /* Here we detect if the current page is from an external documentation and we 
+  change the href of the editing link so it goes to the right repository and file */
+  const editNode = document.querySelector('[title="Edit this page"]')
+  const pathname = window.location.pathname
+  const editURI = 'edit/master/'
+  const removeGitSuffix = function (x) { return x.replace(/\.git$/, '') }
+
+  // Returns the external repo associated to a pathname
+  const detectExternalRepo = function (pathname) {
+    // window.outsideDocs is defined in main.html
+    for (var outsideDoc of window.outsideDocs) {
+      if (window.location.pathname.includes('/' + outsideDoc.name + '/')) {
+        return outsideDoc
+      }
+    }
+  }
+
+  // Change the href attribute of a link to point to the correct edition page on GitHub
+  // for the passed external documentation
+  const fixEditLink = function (editNode, externalDoc) {
+    const inDev = window.location.host.includes('localhost')
+    const prefix = inDev ? '/' : '/en/'
+    const isReadme = document.title.includes('README')
+    const repoFile = pathname.replace(prefix + externalDoc.name, '').replace(/\/$/, isReadme ? '/README.md' : '.md')
+    const baseRepo = removeGitSuffix(externalDoc.repo)
+    const href = baseRepo + '/' + editURI + externalDoc.subdir + repoFile
+    editNode.href = href
+  }
+
+  const externalRepo = detectExternalRepo(pathname)
+  if (externalRepo) {
+    fixEditLink(editNode, externalRepo)
+  }
+}
 
 window.addEventListener('load', function () {
   setupSearchHotkey()
+  fixEditLinkIfNecessary()
 })
