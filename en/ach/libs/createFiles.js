f9763af1c256faf172fb710df430c66f62039c89
Travis CI User <travis@example.org>
2021-04-28 17:23:41 +0000

    publish: fix: Path to access to projects
    generated from commit 7a8bf40ebfd638763906db05789686de803486d2

--- en/ach/libs/createFiles.js
+++ en/ach/libs/createFiles.js
@@ -1,15 +1,13 @@
-const randomWords = require('random-words')
 const ProgressBar = require('progress')
+const faker = require('faker')
 
 module.exports = async (client, path, filesCount) => {
   const { forceCreateByPath } = client.files
 
-  const fileNames = randomWords({ exactly: filesCount })
-
   const bar = new ProgressBar(':bar', { total: filesCount })
 
   for (let i = 0; i < filesCount; i++) {
-    const name = fileNames[i]
+    const name = faker.datatype.uuid()
     await forceCreateByPath(`${path}/${name}.txt`, name, {
       name: `${name}.txt`,
       contentType: 'text/plain'
