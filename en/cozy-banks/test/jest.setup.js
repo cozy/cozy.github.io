6e784cf0c107556e5c878346f4d2bb911ca39702
Travis CI User <travis@example.org>
2021-10-26 16:36:20 +0000

    publish: fix: Use Node 12
    generated from commit d7f15f8319a93c236ff6dca4041261f16d4c1c6a

--- en/cozy-banks/test/jest.setup.js
+++ en/cozy-banks/test/jest.setup.js
@@ -9,6 +9,7 @@ import minilog from '@cozy/minilog'
 // Material-UI: the color provided to augmentColor(color) is invalid.
 // The color object needs to have a `main` property or a `500` property.
 jest.mock('cozy-ui/transpiled/react/utils/color', () => ({
+  ...jest.requireActual('cozy-ui/transpiled/react/utils/color'),
   getCssVariableValue: () => '#fff'
 }))
 
