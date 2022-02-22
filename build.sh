4c9293afbbdfce58272f87ab482073dd6ac9b17a
aeris <aeris@imirhil.fr>
2022-02-22 17:54:04 +0100

    doc: Integrate OpenAPI Cloudery

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 set -e
 
 rm -rf docs/*
-cp index.html docs/
-cp CNAME docs/
+cp index.html CNAME docs/
 
 python generate_config.py --fetch
-
 mkdocs build -f mkdocs.yml
+yarn run redoc-cli bundle --disableGoogleFont src/cloudery/index.yaml -o docs/cloudery/index.html
+cp src/cloudery/index.yaml docs/cloudery/
