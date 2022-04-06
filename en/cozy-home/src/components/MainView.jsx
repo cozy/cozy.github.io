3588647dbe6dcd04504b2ae26430bcb5b6cb5307
Travis CI User <travis@example.org>
2022-04-06 18:13:01 +0000

    publish: doc: Cloudery onboarding
    generated from commit 7d193c71f68f2bdffe96bcb7c8f478fa8321d155

--- en/cozy-home/src/components/MainView.jsx
+++ en/cozy-home/src/components/MainView.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const MainView = ({ children }) => (
-  <main className="main-view u-flex u-flex-column u-flex-content-start u-flex-content-stretch u-w-100 u-m-auto u-pos-relative">
+  <main className="main-view u-flex u-flex-column u-flex-content-start u-flex-content-stretch u-w-100 u-pos-relative">
     {children}
   </main>
 )
