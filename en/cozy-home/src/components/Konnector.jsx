1b75a41bec7f3444adbbdcf44e1e4344d9249bcd
Travis CI User <travis@example.org>
2022-04-16 18:19:16 +0000

    publish: doc: Cloudery onboarding
    generated from commit 7d193c71f68f2bdffe96bcb7c8f478fa8321d155

--- en/cozy-home/src/components/Konnector.jsx
+++ en/cozy-home/src/components/Konnector.jsx
@@ -5,6 +5,7 @@ import flow from 'lodash/flow'
 
 import { Routes as HarvestRoutes } from 'cozy-harvest-lib'
 import datacardOptions from 'cozy-harvest-lib/dist/datacards/datacardOptions'
+import log from 'cozy-logger'
 
 import { getKonnector } from 'ducks/konnectors'
 
@@ -13,10 +14,22 @@ import { getTriggersByKonnector } from 'reducers'
 export const Konnector = ({ konnector, history, triggers }) => {
   const konnectorWithTriggers = { ...konnector, triggers: { data: triggers } }
   const onDismiss = useCallback(() => history.push('/connected'), [history])
+  const slug = konnector?.slug || location.hash.split('/')[2]
+
+  if (!slug) {
+    log(
+      'error',
+      `<Konnector /> failed to render. No Konnector slug was provided to the component props.
+      Tried to recover with window location but couldn't find a slug.
+      Hypertext Reference is "${location.href}".`
+    )
+
+    return null
+  }
 
   return (
     <HarvestRoutes
-      konnectorRoot={`/connected/${konnector.slug}`}
+      konnectorRoot={`/connected/${slug}`}
       konnector={konnectorWithTriggers}
       onDismiss={onDismiss}
       datacardOptions={datacardOptions}
