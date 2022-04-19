52c53e66364c667e78b04f9ce94c0de4d05f0580
Travis CI User <travis@example.org>
2022-04-19 18:21:08 +0000

    publish: doc: Cloudery onboarding
    generated from commit 7d193c71f68f2bdffe96bcb7c8f478fa8321d155

--- en/cozy-apps-registry/go.mod
+++ en/cozy-apps-registry/go.mod
@@ -14,11 +14,11 @@ require (
 	github.com/ncw/swift v1.0.53
 	github.com/onsi/ginkgo v1.15.0 // indirect
 	github.com/onsi/gomega v1.10.5 // indirect
-	github.com/pkg/xattr v0.4.6
+	github.com/pkg/xattr v0.4.7
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.4.0
-	github.com/spf13/viper v1.10.1
+	github.com/spf13/viper v1.11.0
 	github.com/stretchr/testify v1.7.1
-	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
--- en/eslint-config-cozy-app/basics.js
+++ en/eslint-config-cozy-app/basics.js
@@ -1,31 +1,32 @@
 'use strict'
 
 module.exports = {
-  plugins: ['prettier', 'promise'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'eslint-config-prettier',
     'plugin:promise/recommended'
   ],
   parser: '@babel/eslint-parser',
-  env: {
-    browser: true,
-    jest: true,
-    node: true,
-    es6: true
-  },
+  plugins: ['prettier', 'promise'],
   rules: {
     'no-console': 'error',
+    'no-param-reassign': 'warn',
     'prettier/prettier': [
       'error',
       {
-        singleQuote: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
         semi: false,
-        trailingComma: 'none',
-        arrowParens: 'avoid'
+        singleQuote: true,
+        trailingComma: 'none'
       }
     ],
-    'no-param-reassign': 'warn',
-    'spaced-comment': ['error', 'always']
+    'spaced-comment': ['error', 'always', { block: { exceptions: ['*'] } }]
   }
 }
