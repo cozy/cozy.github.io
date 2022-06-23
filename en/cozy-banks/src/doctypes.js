7451aee51ec12a9f012930613f013c5be053ac3d
Travis CI User <travis@example.org>
2022-06-23 18:54:26 +0000

    publish: doc: Cloudery onboarding
    generated from commit 7d193c71f68f2bdffe96bcb7c8f478fa8321d155

--- en/cozy-banks/src/doctypes.js
+++ en/cozy-banks/src/doctypes.js
@@ -207,8 +207,7 @@ export const cronKonnectorTriggersConn = {
       worker: 'konnector',
       type: '@cron'
     }),
-  as: 'triggers',
-  fetchPolicy: older30s
+  as: 'triggers'
 }
 
 export const transactionsConn = {
