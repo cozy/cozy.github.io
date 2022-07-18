f2bf5415fab383ba3a33fbd310ff22bc419d1101
Travis CI User <travis@example.org>
2022-07-18 19:10:56 +0000

    publish: doc: Cloudery onboarding
    generated from commit 7d193c71f68f2bdffe96bcb7c8f478fa8321d155

--- en/cozy-apps-registry/Makefile
+++ en/cozy-apps-registry/Makefile
@@ -30,7 +30,7 @@ lint: bin/golangci-lint
 .PHONY: lint
 
 bin/golangci-lint: Makefile
-	@curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- v1.46.2
+	@curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- v1.47.0
 
 ## tests: run the tests
 tests:
--- en/cozy-banks/src/targets/services/recurrence.js
+++ en/cozy-banks/src/targets/services/recurrence.js
@@ -1,6 +1,15 @@
 import { runService } from './service'
-import runRecurrenceService from 'ducks/recurrence/service'
+import logger from 'cozy-logger'
 
-runService(async ({ client }) => {
-  await runRecurrenceService({ client })
+const log = logger.namespace('recurrence')
+// import runRecurrenceService from 'ducks/recurrence/service'
+
+runService(async (/* { client }*/) => {
+  // FIXME: Find out why this service loops (i.e. modifies transactions which
+  // re-triggers it).
+  log(
+    'debug',
+    'Not running the service as it keeps modifying transactions triggering further service calls'
+  )
+  // await runRecurrenceService({ client })
 })
