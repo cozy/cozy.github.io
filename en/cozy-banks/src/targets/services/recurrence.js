4225beeab2295a560ac6c4d704009881e7cac93f
Travis CI User <travis@example.org>
2022-08-03 19:20:47 +0000

    publish: docs: Add HowTo to run connectors with local stack
    generated from commit 9e4b51e7f1dbb62beec4dc3391347d8942a8493d

--- en/cozy-banks/src/targets/services/recurrence.js
+++ en/cozy-banks/src/targets/services/recurrence.js
@@ -1,6 +1,20 @@
-import { runService } from './service'
+import flag from 'cozy-flags'
+
+import { logger } from 'ducks/konnectorAlerts'
 import runRecurrenceService from 'ducks/recurrence/service'
+import { runService } from './service'
 
 runService(async ({ client }) => {
+  client.registerPlugin(flag.plugin)
+  await client.plugins.flags.refresh()
+
+  if (!flag('banks.services.recurrence.enabled')) {
+    logger(
+      'info',
+      'Bailing out of recurrence service since flag "banks.services.recurrence.enabled" is not set'
+    )
+    return
+  }
+
   await runRecurrenceService({ client })
 })
