709798f8d67994561733787241ab50efb5cb4c0e
Travis CI User <travis@example.org>
2023-04-24 21:32:51 +0000

    publish: Merge pull request #259 from cozy/NoRelaunchForKonnectors
    generated from commit 2f6a9fa92757ed8448ae348ffa5fe92e0f172053

--- en/cozy-home/app.config.js
+++ en/cozy-home/app.config.js
@@ -4,4 +4,12 @@ const configs = [
   require('./config/webpack.config.cozy-home.js')
 ]
 
+const extraConfig = {
+  resolve: {
+    alias: {
+      'react-pdf$': 'react-pdf/dist/esm/entry.webpack'
+    }
+  }
+}
+configs.push(extraConfig)
 module.exports = configs
