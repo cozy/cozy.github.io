a7bfc7cd3f6cad6964b66be2de2f09ffc882054f
Travis CI User <travis@example.org>
2023-07-06 22:01:49 +0000

    publish: Update golang version for stack build
    generated from commit 1eb9a3d0ca7a4cb4f90a43fd9c2a9e9cf37cd0e4

--- en/cozy-home/src/components/FallbackCandidateServiceTile.jsx
+++ en/cozy-home/src/components/FallbackCandidateServiceTile.jsx
@@ -17,7 +17,14 @@ const FallbackCandidateServiceTile = ({ slug }) => {
     ? get(registryData, 'latest_version.manifest.name', slug)
     : ''
 
-  return (
+  /**
+   * If there is no registry data it is better to render nothing because we won't have any link to the Konnector, despite having its slug.
+   * This is because failing to get the data most likely means that the Service is not available on the current registry (404 Not Found).
+   *
+   * It is not a problem if the Service is not available on the registry, but it is a problem if we try to link to it.
+   * Here it would display an AppLinker with a grey cube, an unformatted slug as a name and no working link.
+   */
+  return registryData ? (
     <AppLinker
       app={{ slug: app }}
       nativePath={nativePath}
@@ -40,7 +47,7 @@ const FallbackCandidateServiceTile = ({ slug }) => {
         </a>
       )}
     </AppLinker>
-  )
+  ) : null
 }
 
 FallbackCandidateServiceTile.propTypes = {
