efa357592784696206b204e6412eb973320ddc14
Travis CI User <travis@example.org>
2023-07-26 22:07:11 +0000

    publish: docs: Fix selfhosting doc for recent stack version
    generated from commit 3d6f0b643430c0f5e7f596270035cf16b22aa57d

--- en/cozy-home/src/components/BackupNotification/BackupNotification.jsx
+++ en/cozy-home/src/components/BackupNotification/BackupNotification.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
+import { useClient, generateWebLink } from 'cozy-client'
 import { isFlagshipApp } from 'cozy-device-helper'
 import { useWebviewIntent } from 'cozy-intent'
 
 import CozyTheme from 'cozy-ui/transpiled/react/CozyTheme'
+import AppLinker from 'cozy-ui/transpiled/react/AppLinker'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import ListItem from 'cozy-ui/transpiled/react/ListItem'
@@ -21,6 +23,7 @@ import { useBackupData } from 'components/BackupNotification/useBackupData'
 import styles from 'styles/backupNotification.styl'
 
 const BackupNotification = () => {
+  const client = useClient()
   const { t } = useI18n()
   const [isConfirmStopBackupDialogOpen, setIsConfirmStopBackupDialogOpen] =
     useState(false)
@@ -49,39 +52,72 @@ const BackupNotification = () => {
     setIsConfirmStopBackupDialogOpen(false)
   }
 
+  const cozyURL = new URL(client.getStackClient().uri)
+  const app = 'photos'
+  const nativePath = '/backup'
+  const { subdomain: subDomainType } = client.getInstanceOptions()
+
   return (
     <div>
       <CozyTheme variant="inverted">
-        <div className={styles['backup-notification-wrapper']}>
-          <ListItem component="div" ContainerComponent="div">
-            <ListItemIcon className="u-pos-relative">
-              <Icon icon={PhoneUploadIcon} size={12} />
-              <CircularProgress
-                variant="determinate"
-                value={backupProgressPercentage}
-                size={32}
-                className="u-pos-absolute"
-              />
-            </ListItemIcon>
-            <ListItemText
-              primary={
-                <Typography variant="body1" className="u-primaryTextColor">
-                  {t('backup.backupInProgress')}
-                </Typography>
-              }
-              secondary={`${
-                totalMediasToBackupCount - mediasToBackupCount
-              } / ${totalMediasToBackupCount}`}
-            />
-            <ListItemSecondaryAction
-              onClick={() => setIsConfirmStopBackupDialogOpen(true)}
-            >
-              <ListItemIcon className="u-mr-half">
-                <Icon icon={CrossCircleOutlineIcon} size={16} />
-              </ListItemIcon>
-            </ListItemSecondaryAction>
-          </ListItem>
-        </div>
+        <AppLinker
+          app={{ slug: app }}
+          nativePath={nativePath}
+          href={generateWebLink({
+            pathname: '/',
+            cozyUrl: cozyURL.origin,
+            slug: app,
+            hash: nativePath,
+            subDomainType
+          })}
+        >
+          {({ onClick, href }) => (
+            <div className={styles['backup-notification-wrapper']}>
+              <ListItem
+                component="a"
+                ContainerComponent="div"
+                onClick={onClick}
+                href={href}
+              >
+                <ListItemIcon className="u-pos-relative">
+                  <Icon icon={PhoneUploadIcon} size={12} />
+                  <CircularProgress
+                    variant="determinate"
+                    value={backupProgressPercentage}
+                    size={32}
+                    className="u-pos-absolute"
+                  />
+                  <CircularProgress
+                    variant="determinate"
+                    value={100}
+                    size={32}
+                    className="u-pos-absolute"
+                    classes={{
+                      svg: styles['backup-circular-progress-background']
+                    }}
+                  />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <Typography variant="body1" className="u-primaryTextColor">
+                      {t('backup.backupInProgress')}
+                    </Typography>
+                  }
+                  secondary={`${
+                    totalMediasToBackupCount - mediasToBackupCount
+                  } / ${totalMediasToBackupCount}`}
+                />
+                <ListItemSecondaryAction
+                  onClick={() => setIsConfirmStopBackupDialogOpen(true)}
+                >
+                  <ListItemIcon className="u-mr-half">
+                    <Icon icon={CrossCircleOutlineIcon} size={16} />
+                  </ListItemIcon>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </div>
+          )}
+        </AppLinker>
       </CozyTheme>
       <CozyTheme variant="normal">
         {isConfirmStopBackupDialogOpen && (
