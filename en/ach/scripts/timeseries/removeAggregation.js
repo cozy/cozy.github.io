69418478dd1ce3dc80947dc3898967d4906cf787
Travis CI User <travis@example.org>
2023-09-18 22:29:21 +0000

    publish: docs: Fix selfhosting doc for recent stack version
    generated from commit 3d6f0b643430c0f5e7f596270035cf16b22aa57d

---
+++ en/ach/scripts/timeseries/removeAggregation.js
@@ -0,0 +1,46 @@
+const { Q } = require('cozy-client')
+
+module.exports = {
+  getDoctypes: function() {
+    return ['io.cozy.timeseries.geojson']
+  },
+  /**
+   * FIXME: The params are an array passed to the scripts, here used to get the startDate
+   * This seems rather fragile, and cannot be documented in the CLI
+   * We need a better way to handle specific params for scripts
+   */
+
+  run: async function(ach, dryRun = true, params) {
+    const client = ach.client
+    if (dryRun) {
+      console.log('This is a dry run, no doc will be updated')
+    }
+
+    const fromDate = params.length > 0 ? params[0] : null
+
+    console.log(`Looking for docs with startDate above ${fromDate}`)
+    const query = Q('io.cozy.timeseries.geojson')
+      .where({
+        aggregation: { $exists: true },
+        startDate: {
+          $gt: fromDate
+        }
+      })
+      .limitBy(null)
+    const docs = await client.queryAll(query)
+    let newDocs = []
+    if (dryRun) {
+      console.log(`${docs.length} docs would be updated.`)
+      return
+    }
+    console.log(`Remove ${docs.length} aggregation from timeseries...`)
+    for (const doc of docs) {
+      delete doc.aggregation
+      newDocs.push(doc)
+    }
+    if (newDocs.length > 0) {
+      await client.saveAll(newDocs)
+    }
+    console.log('Done!')
+  }
+}
