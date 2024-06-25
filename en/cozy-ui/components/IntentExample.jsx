576d179d9ac59f4574638ccd4f5d2fcc2d0e58c0
Travis CI User <travis@example.org>
2024-06-25 01:50:00 +0000

    publish: Add Ubuntu 24.04 cozy-stack packages
    generated from commit b2d4b2f747ce7c82f255e97a232f142b1a768c58

--- en/cozy-ui/components/IntentExample.jsx
+++ en/cozy-ui/components/IntentExample.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
+
 import IntentWrapper from '../../react/IntentWrapper'
+import CozyTheme from '../../react/providers/CozyTheme'
+import Button from '../../react/Buttons'
+import Typography from '../../react/Typography'
 import { placeholder90 } from '../placeholders/img'
 
 const IntentExample = function({ onComplete, action, doctype, options }) {
   return (
-    <IntentWrapper
-      appIcon={placeholder90}
-      appName="IntentExample"
-      appEditor="EditorExample"
-    >
-      <p>
-        Action: {action}
-        <br />
-        Doctype: {doctype}
-        <br />
-        Options: <pre>{JSON.stringify(options, null, 2)}</pre>
-        <br />
-        <br />
-        <button onClick={onComplete}>Click to complete intent</button>
-      </p>
-    </IntentWrapper>
+    <CozyTheme>
+      <IntentWrapper
+        appIcon={placeholder90}
+        appName="IntentExample"
+        appEditor="EditorExample"
+      >
+        <Typography>Action: {action}</Typography>
+        <Typography>Doctype: {doctype}</Typography>
+        <Typography>
+          Options: <pre>{JSON.stringify(options, null, 2)}</pre>
+        </Typography>
+        <Button
+          className="u-mt-1"
+          label="Click to complete intent"
+          onClick={onComplete}
+        />
+      </IntentWrapper>
+    </CozyTheme>
   )
 }
 
